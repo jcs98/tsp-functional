b8528b7a3073ad8497bdd7c6c3148744d32d361b
Jainam Chirag Shah <jcshah98@gmail.com>
2021-12-05 16:17:12 -0500

    Write path distance function

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,24 @@
 module Lib
-    ( someFunc
+    ( runMain
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "Hello"
+type Point = (Int, Int)
+
+squaredDistance :: Point -> Point -> Int
+squaredDistance (x1, y1) (x2, y2) = ((x2 - x1) ^ 2) + ((y2 - y1) ^ 2)
+
+distance :: Point -> Point -> Int
+distance a b = floor . sqrt . fromIntegral $ squaredDistance a b
+
+makeCities :: String -> [Point]
+makeCities corpus = makePairs $ map read $ words corpus
+    where
+        makePairs [] = []
+        makePairs (p:q:r) = (p, q):(makePairs r)
+
+pathDistance :: [Point] -> Int
+pathDistance cities = sum $ zipWith distance path (tail path)
+    where path = (last cities):cities 
+
+runMain :: IO ()
+runMain = putStrLn $ show $ pathDistance $ makeCities $ "0 0 3 4 6 8"
