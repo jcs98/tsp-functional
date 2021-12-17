8336f5a8cdeb5997c2b39e58e933bf8befdf7000
Jainam Chirag Shah <jcshah98@gmail.com>
2021-12-17 00:34:30 -0500

    Linting

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,12 +1,13 @@
 module Lib
-    ( runMain
-    ) where
+  ( runMain,
+  )
+where
 
-import System.IO(readFile)
-import System.Environment(getArgs, getProgName)
-import System.Exit(die)
-import Data.List(permutations)
 import Control.Parallel.Strategies
+import Data.List (permutations)
+import System.Environment (getArgs, getProgName)
+import System.Exit (die)
+import System.IO (readFile)
 
 type Point = (Int, Int)
 
@@ -18,41 +19,52 @@ distance a b = floor . sqrt . fromIntegral $ squaredDistance a b
 
 makeCities :: String -> [Point]
 makeCities corpus = makePairs $ map read $ words corpus
-    where
-        makePairs [] = []
-        makePairs [p] = [(p, p)] -- replicate last coordinate if odd numbers
-        makePairs (p:q:r) = (p, q) : makePairs r
+  where
+    makePairs [] = []
+    makePairs [p] = [(p, p)] -- replicate last coordinate if odd numbers
+    makePairs (p : q : r) = (p, q) : makePairs r
 
 pathDistance :: [Point] -> Int
 pathDistance cities = sum $ zipWith distance path (tail path)
-    where path = last cities : cities
+  where
+    path = last cities : cities
 
 -- Consider all permutations while keeping starting point fixed
 minPathDistance :: [Point] -> Int
 minPathDistance [] = -1
-minPathDistance (c:cities) = minimum $ map (pathDistance . (c :)) $ permutations cities
+minPathDistance (c : cities) =
+  minimum $
+    map (pathDistance . (c :)) $
+      permutations cities
 
 parallelMinPathDistance :: [Point] -> Int
 parallelMinPathDistance [] = -1
-parallelMinPathDistance (c:cities) = minimum $ parMap rdeepseq (pathDistance . (c :)) $ permutations cities
+parallelMinPathDistance (c : cities) =
+  minimum $
+    parMap rdeepseq (pathDistance . (c :)) $
+      permutations cities
 
 chunkedParallelMinPathDistance :: [Point] -> Int -> Int
 chunkedParallelMinPathDistance [] _ = -1
-chunkedParallelMinPathDistance (c:cities) chunkSize = minimum $ withStrategy (parListChunk chunkSize rdeepseq) . map (pathDistance . (c :)) $ permutations cities
+chunkedParallelMinPathDistance (c : cities) chunkSize =
+  minimum $
+    withStrategy (parListChunk chunkSize rdeepseq)
+      . map (pathDistance . (c :))
+      $ permutations cities
 
 runMain :: IO ()
 runMain = do
-   args <- getArgs
-   case args of
-      ["-s", filename] -> do
-        corpus <- readFile filename
-        print $ minPathDistance $ makeCities corpus
-      ["-p", filename] -> do
-        corpus <- readFile filename
-        print $ parallelMinPathDistance $ makeCities corpus
-      ['-':'c':n, filename] -> do
-        corpus <- readFile filename
-        print $ chunkedParallelMinPathDistance (makeCities corpus) (read n)
-      _ -> do
-         pn <- getProgName
-         die $ "Usage: " ++ pn ++ " [-s|-p|-cN] <filename>"
+  args <- getArgs
+  case args of
+    ["-s", filename] -> do
+      corpus <- readFile filename
+      print $ minPathDistance $ makeCities corpus
+    ["-p", filename] -> do
+      corpus <- readFile filename
+      print $ parallelMinPathDistance $ makeCities corpus
+    ['-' : 'c' : n, filename] -> do
+      corpus <- readFile filename
+      print $ chunkedParallelMinPathDistance (makeCities corpus) (read n)
+    _ -> do
+      pn <- getProgName
+      die $ "Usage: " ++ pn ++ " [-s|-p|-cN] <filename>"
