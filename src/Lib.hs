977127ab74ad35f7b92f06e934031a35f1815196
Jainam Chirag Shah <jcshah98@gmail.com>
2021-12-19 02:34:14 -0500

    Cleanup

--- src/Lib.hs
+++ src/Lib.hs
@@ -20,17 +20,11 @@ minPathDistance (c : cities) =
     map (pathDistance . (c :)) $
       permutations cities
 
-batchMinPathDistance :: [[Point]] -> [Int]
-batchMinPathDistance = map minPathDistance
-
-parallelBatchMinPathDistance :: [[Point]] -> [Int]
-parallelBatchMinPathDistance = parMap rseq minPathDistance
-
 parallelMinPathDistance :: [Point] -> Int
 parallelMinPathDistance [] = -1
 parallelMinPathDistance (c : cities) =
   minimum $
-    parMap rdeepseq (pathDistance . (c :)) $
+    parMap rseq (pathDistance . (c :)) $
       permutations cities
 
 chunkedParallelMinPathDistance :: [Point] -> Int -> Int
@@ -41,42 +35,90 @@ chunkedParallelMinPathDistance (c : cities) chunkSize =
       . map (pathDistance . (c :))
       $ permutations cities
 
-geneticMinPathDistance :: [Point] -> Int -> Int -> Int
-geneticMinPathDistance [] _ _ = -1
-geneticMinPathDistance cities popSize rounds =
+batchMinPathDistance :: [[Point]] -> [Int]
+batchMinPathDistance = map minPathDistance
+
+batchParallelMinPathDistance :: [[Point]] -> [Int]
+batchParallelMinPathDistance = parMap rseq minPathDistance
+
+geneticMinPathDistance :: Int -> Int -> [Point] -> Int
+geneticMinPathDistance _ _ [] = -1
+geneticMinPathDistance populationSize generations cities =
   minimum $ map pathDistance finalPop
   where
-    population = replicate popSize cities
+    population = replicate populationSize cities
     randomList = randomListInRange 0 (length cities - 1)
     finalPop =
       fst $
         foldr
           (\f (p, r) -> (f p r, tail r))
           (population, randomList)
-          (replicate rounds nextGen)
+          (replicate generations nextGen)
+
+batchGeneticMinPathDistance :: Int -> Int -> [[Point]] -> [Int]
+batchGeneticMinPathDistance p g =
+  map
+    (geneticMinPathDistance p g)
+
+batchParallelGeneticMinPathDistance :: Int -> Int -> [[Point]] -> [Int]
+batchParallelGeneticMinPathDistance p g =
+  parMap
+    rseq
+    (geneticMinPathDistance p g)
 
 runMain :: IO ()
 runMain = do
   args <- getArgs
   case args of
+    -- bruteforce sequential
     ["-s", filename] -> do
       corpus <- readFile filename
       print $ minPathDistance $ makeCities corpus
-    ["--batch", "-s", filename] -> do
-      corpus <- readFile filename
-      print $ batchMinPathDistance $ replicate 16 $ makeCities corpus
-    ["--batch", "-p", filename] -> do
-      corpus <- readFile filename
-      print $ parallelBatchMinPathDistance $ replicate 16 $ makeCities corpus
+
+    -- bruteforce, calculate path distance in parallel
     ["-p", filename] -> do
       corpus <- readFile filename
       print $ parallelMinPathDistance $ makeCities corpus
+
+    -- bruteforce, calculate path distance in parallel chunks
     ["-c", ':' : 'n' : n, filename] -> do
       corpus <- readFile filename
       print $ chunkedParallelMinPathDistance (makeCities corpus) (read n)
-    ["-g", ':' : 's' : s, ':' : 'r' : r, filename] -> do
+
+    -- bruteforce for batch of city groups
+    ["-s", ':' : 'b' : b, filename] -> do
       corpus <- readFile filename
-      print $ geneticMinPathDistance (makeCities corpus) (read s) (read r)
+      let cities = makeCities corpus
+          randomList = randomListInRange 0 (length cities)
+       in print $ batchMinPathDistance [take r cities | r <- take (read b) randomList]
+
+    -- bruteforce for batch of city groups, each group in parallel
+    ["-sp", ':' : 'b' : b, filename] -> do
+      corpus <- readFile filename
+      let cities = makeCities corpus
+          randomList = randomListInRange 0 (length cities)
+       in print $ batchParallelMinPathDistance [take r cities | r <- take (read b) randomList]
+
+    -- genetic algorithm
+    ["-g", ':' : 'p' : p, ':' : 'g' : g, filename] -> do
+      corpus <- readFile filename
+      print $ geneticMinPathDistance (read p) (read g) $ makeCities corpus
+
+    -- genetic algorithm for batch of city gorups
+    ["-g", ':' : 'p' : p, ':' : 'g' : g, ':' : 'b' : b, filename] -> do
+      corpus <- readFile filename
+      let cities = makeCities corpus
+          randomList = randomListInRange 0 (length cities)
+       in print $ batchGeneticMinPathDistance (read p) (read g) [take r cities | r <- take (read b) randomList]
+
+    -- genetic algorithm for batch of city gorups, each group in parallel
+    ["-gp", ':' : 'p' : p, ':' : 'g' : g, ':' : 'b' : b, filename] -> do
+      corpus <- readFile filename
+      let cities = makeCities corpus
+          randomList = randomListInRange 0 (length cities)
+       in print $ batchParallelGeneticMinPathDistance (read p) (read g) [take r cities | r <- take (read b) randomList]
+
+    -- invalid running params
     _ -> do
       pn <- getProgName
       die $ "Usage: " ++ pn ++ " [-s|-p|-c :nN|-g :sN :rN] <filename>"
